0debf1d0c878fa6c854e9b27efb57aa971c8f513
Abel <abelromeror763@gmil.com>
2020-02-11 09:51:35 -0600

    Funciones nuevas de catalogos: - Colonias (Agregar,Editar,Eliminar). - Municipios (Agregar,Editar,Eliminar).
    Cambio en el header para agregar municipios y colonias.

---
+++ catalogos/procesos/colonias/colonia_update.php
@@ -0,0 +1,13 @@
+<?php
+
+include('../conexion.php');
+
+$name = $_POST['name'];
+$id = $_POST['id'];
+$sql = "CALL sp_EditarColonia('".$id."','".$name."');";
+
+$resultado = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($resultado);
+
+echo $row['msg'];
+?>
